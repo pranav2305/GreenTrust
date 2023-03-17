c9481f44afd1a85f17beeb4c92a833dc56d4cf47
Pranav <pranav2305@gmail.com>
2023-03-17 10:20:24 +0530

    Clean file structure

--- tasks/printAccounts.js
+++
@@ -1,6 +0,0 @@
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-    for (const account of accounts) {
-      console.log(account.address);
-    }
-  });
